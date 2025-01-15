9de7b76eca2f51980be7de0bbe1cf10180cfb25d
rianaalexia <cocotian.riana@gmail.com>
2025-01-15 23:10:53 +0200

    orders si manager orders

---
+++ SmartPrintHub/Orders/Order.cs
@@ -0,0 +1,18 @@
+﻿public class Order
+{
+    public string Name { get; private set; }
+    public string ObjectName { get; private set; }
+    public float weight { get; private set; }
+    public string PrinterTypename { get; private set; }
+    public string destination { get; private set; }
+    public float price { get; private set; }
+    public Order(string name,string objectName, float weight,string printerType, string destination, float price)
+    {
+        Name = name;
+        PrinterTypename = printerType;
+        ObjectName = objectName;
+        this.weight = weight;
+        this.destination = destination;
+        this.price = price;
+    }
+}
---
+++ SmartPrintHub/Orders/OrderManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+
+public class OrderManager
+{
+    private List<Order> orders = new List<Order>();
+    public void AddOrder(Order order)
+    {
+        orders.Add(order);
+    }
+    public Order getOrder()
+    {
+        Order temp = orders[orders.Count()-1];
+        if (orders.Count() > 0)
+        {
+            orders.RemoveAt(orders.Count() - 1);
+            return temp;
+        }
+        return null;
+    }
+    public void LoadFromJson()
+    {
+        var options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+        string jsonString = File.ReadAllText("orders.json");
+        orders = JsonSerializer.Deserialize<List<Order>>(jsonString, options);
+    }
+
+    public void SaveToJson()
+    {
+        string jsonString = JsonSerializer.Serialize(orders);
+        File.WriteAllText("orders.json", jsonString);
+    }
+
+}
