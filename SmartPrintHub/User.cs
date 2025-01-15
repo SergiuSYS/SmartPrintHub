5d7f897de8a44c70cea3747fe0b4459db96b277c
Sergiu <Sergiusay06@gmail.com>
2025-01-15 23:45:26 +0200

    UserPage

--- SmartPrintHub/User.cs
+++ SmartPrintHub/User.cs
@@ -7,24 +7,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace WinFormsApp1
 {
     public partial class User : Form
     {
         CalculatePrice calculatePrice = new CalculatePrice();
+        OrderManager orderManager = new OrderManager();
         public User()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
+            if (float.TryParse(textBox2.Text, out float weight))
+            {
+                string printerType = textBox3.Text;
+
+                if (!string.IsNullOrEmpty(printerType))
+                {
+                    float price = calculatePrice.Calculate(weight, printerType);
+                    label9.Text = price.ToString("F2");
+                }
+                else
+                {
+                    label9.Text = "Enter a printer type.";
+                }
+            }
+            else
+            {
+                label9.Text = "Invalid weight.";
+            }
+            label9.Refresh();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            orderManager.AddOrder(new Order(textBox6.Text, textBox1.Text, float.Parse(textBox2.Text), textBox3.Text, textBox4.Text,float.Parse(label9.Text)));
+            orderManager.SaveToJson();
         }
     }
+    
 }
