a0c2a899ea18c4d6e7cc6e1c4d0838c33ce1df98
rianaalexia <cocotian.riana@gmail.com>
2025-01-16 10:29:12 +0200

    design user

--- SmartPrintHub/User.Designer.cs
+++ SmartPrintHub/User.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(User));
             textBox1 = new TextBox();
             textBox2 = new TextBox();
             textBox3 = new TextBox();
@@ -42,131 +43,180 @@
             label7 = new Label();
             label8 = new Label();
             label9 = new Label();
+            pictureBox3 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
             SuspendLayout();
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(360, 110);
+            textBox1.BackColor = Color.FromArgb(224, 205, 248);
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Location = new Point(411, 194);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
+            textBox1.Size = new Size(114, 27);
             textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(360, 196);
+            textBox2.BackColor = Color.FromArgb(224, 205, 248);
+            textBox2.BorderStyle = BorderStyle.FixedSingle;
+            textBox2.Location = new Point(411, 308);
+            textBox2.Margin = new Padding(3, 4, 3, 4);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(100, 23);
+            textBox2.Size = new Size(114, 27);
             textBox2.TabIndex = 1;
             textBox2.TextChanged += textBox2_TextChanged;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(360, 154);
+            textBox3.BackColor = Color.FromArgb(224, 205, 248);
+            textBox3.BorderStyle = BorderStyle.FixedSingle;
+            textBox3.Location = new Point(411, 252);
+            textBox3.Margin = new Padding(3, 4, 3, 4);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(100, 23);
+            textBox3.Size = new Size(114, 27);
             textBox3.TabIndex = 2;
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(360, 238);
+            textBox4.BackColor = Color.FromArgb(224, 205, 248);
+            textBox4.BorderStyle = BorderStyle.FixedSingle;
+            textBox4.Location = new Point(411, 364);
+            textBox4.Margin = new Padding(3, 4, 3, 4);
             textBox4.Name = "textBox4";
-            textBox4.Size = new Size(100, 23);
+            textBox4.Size = new Size(114, 27);
             textBox4.TabIndex = 3;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(264, 110);
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Tahoma", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(285, 196);
             label1.Name = "label1";
-            label1.Size = new Size(74, 15);
+            label1.Size = new Size(107, 21);
             label1.TabIndex = 5;
-            label1.Text = "nume obiect";
+            label1.Text = "Object Name";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(271, 199);
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Tahoma", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(310, 310);
             label2.Name = "label2";
-            label2.Size = new Size(52, 15);
+            label2.Size = new Size(62, 21);
             label2.TabIndex = 6;
-            label2.Text = "Greutate";
+            label2.Text = "Weight";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(249, 154);
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Tahoma", 10F);
+            label3.Location = new Point(285, 252);
             label3.Name = "label3";
-            label3.Size = new Size(89, 15);
+            label3.Size = new Size(100, 21);
             label3.TabIndex = 7;
-            label3.Text = "Tip imprimanta";
+            label3.Text = "Printer Type";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(264, 246);
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Tahoma", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(299, 366);
             label4.Name = "label4";
-            label4.Size = new Size(59, 15);
+            label4.Size = new Size(95, 21);
             label4.TabIndex = 8;
-            label4.Text = "Destinatie";
+            label4.Text = "Destination";
             // 
             // button1
             // 
-            button1.Location = new Point(300, 327);
+            button1.BackColor = Color.FromArgb(224, 205, 248);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Tempus Sans ITC", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(343, 429);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(160, 50);
+            button1.Size = new Size(183, 67);
             button1.TabIndex = 10;
-            button1.Text = "CreateComanda";
-            button1.UseVisualStyleBackColor = true;
+            button1.Text = "Add Order";
+            button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click;
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(370, 27);
+            label6.BackColor = Color.Transparent;
+            label6.Font = new Font("Tempus Sans ITC", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(246, 48);
             label6.Name = "label6";
-            label6.Size = new Size(38, 15);
+            label6.Size = new Size(410, 45);
             label6.TabIndex = 11;
-            label6.Text = "label6";
+            label6.Text = "PLEASE ADD AN ORDER";
             // 
             // textBox6
             // 
-            textBox6.Location = new Point(360, 71);
+            textBox6.BackColor = Color.FromArgb(224, 205, 248);
+            textBox6.BorderStyle = BorderStyle.FixedSingle;
+            textBox6.Location = new Point(411, 142);
+            textBox6.Margin = new Padding(3, 4, 3, 4);
             textBox6.Name = "textBox6";
-            textBox6.Size = new Size(100, 23);
+            textBox6.Size = new Size(114, 27);
             textBox6.TabIndex = 12;
             // 
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(264, 71);
+            label7.BackColor = Color.Transparent;
+            label7.Font = new Font("Tahoma", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.Location = new Point(343, 142);
             label7.Name = "label7";
-            label7.Size = new Size(40, 15);
+            label7.Size = new Size(53, 21);
             label7.TabIndex = 13;
-            label7.Text = "Nume";
+            label7.Text = "Name";
             // 
             // label8
             // 
             label8.AutoSize = true;
-            label8.Location = new Point(578, 79);
+            label8.BackColor = Color.Transparent;
+            label8.Font = new Font("Tempus Sans ITC", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label8.Location = new Point(618, 177);
             label8.Name = "label8";
-            label8.Size = new Size(91, 15);
+            label8.Size = new Size(249, 30);
             label8.TabIndex = 14;
-            label8.Text = "pretul Comenzii";
+            label8.Text = "YOUR ORDER PRICE IS:";
             // 
             // label9
             // 
             label9.AutoSize = true;
-            label9.Location = new Point(600, 118);
+            label9.BackColor = Color.Transparent;
+            label9.Location = new Point(708, 227);
             label9.Name = "label9";
-            label9.Size = new Size(38, 15);
+            label9.Size = new Size(78, 20);
             label9.TabIndex = 15;
-            label9.Text = "label9";
+            label9.Text = "orderprice";
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = Color.Transparent;
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(12, 1);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(193, 206);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 16;
+            pictureBox3.TabStop = false;
             // 
             // User
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 436);
+            BackgroundImage = SmartPrintHub.Properties.Resources.background2loin;
+            ClientSize = new Size(914, 581);
+            Controls.Add(pictureBox3);
             Controls.Add(label9);
             Controls.Add(label8);
             Controls.Add(label7);
@@ -181,9 +231,11 @@
             Controls.Add(textBox3);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "User";
             Text = "User";
             Load += User_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -204,5 +256,6 @@
         private Label label7;
         private Label label8;
         private Label label9;
+        private PictureBox pictureBox3;
     }
 }
